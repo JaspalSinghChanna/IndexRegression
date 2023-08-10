a37cb4ae0b63c469164e42159cac45a39594646b
Jaspal Channa <jsc212@pm.me>
2023-08-10 01:03:29 +0100

    render-dash deply

--- IndexRegression/data_loader.py
+++ IndexRegression/data_loader.py
@@ -4,15 +4,30 @@ import pandas as pd
 import yfinance as yf
 from pymongo import MongoClient
 import logging
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+import datetime
 
 INDEX_TICKERS = ['^GSPC','^RUT','^NDX']
 
 class MongoDBConnection:
-    def __init__(self, port=27017):
-        self.client = MongoClient('localhost', port)
+    def __init__(self, env_var = 'MONGO0'):
+        pwd = os.getenv(env_var)
+        uri = f"mongodb+srv://jsc:{pwd}@serverlessinstance0.qecvssp.mongodb.net/?retryWrites=true&w=majority"
+        self.client = MongoClient(uri, server_api=ServerApi('1'))
+        self.ping_db()
         self.db = self.client['IndexRegression']
         self.coll = self.db['CloseData']
 
+    def ping_db(self):
+        # Send a ping to confirm a successful connection
+        try:
+            self.client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            raise Exception('Failed to ping Mongo DB') from e
+        
 class DataDownloader:
     def __init__(self, path='Tickers.csv'):
         self.Mongo = MongoDBConnection()
@@ -58,8 +73,14 @@ class DataDownloader:
 
 if __name__ == '__main__':
     dd = DataDownloader()
+    print(datetime.datetime.now())
     tickers = dd.get_ticker_list()
+    print(datetime.datetime.now())
     dd.delete_values()
+    print(datetime.datetime.now())
     df = dd.download_close_data(tickers)
+    print(datetime.datetime.now())
     dd.insert_into_db(df)
+    print(datetime.datetime.now())
     dd.create_indices()
+    print(datetime.datetime.now())
